435c89c09e9077701963202d565a89856e4020ef
Drew Sommer <drew@sommerengineering.com>
2024-09-28 17:47:58 +0300

    style: Color status bar

--- app/src/main/java/com/example/reply/ui/theme/Theme.kt
+++ app/src/main/java/com/example/reply/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.reply.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
 private val LightColors = lightColorScheme(
@@ -77,7 +82,7 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun AppTheme(
-    useDarkTheme: Boolean = false, // isSystemInDarkTheme(),
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
 
@@ -92,6 +97,20 @@ fun AppTheme(
         }
         useDarkTheme -> DarkColors
         else -> LightColors
+    }
+
+    // color status bar
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.primary.toArgb()
+
+            // toggle dark mode
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = useDarkTheme
+        }
     }
 
     MaterialTheme(
